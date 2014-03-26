f081a1ee7693d432575b18b862c75818a858f099
Chamath Jeevan <chamathjeevan@gmail.com>
2014-03-26 19:19:25 +0530

    RUSH Training Exercises 2. Coding skills - "print number"

---
+++ PrintNumbers/NumberToText.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PrintNumbers
+{
+    public static class NumberToText
+    {
+        private static string EnglishTextNumber;
+
+
+        static Dictionary<int, string> texTDictionary = new Dictionary<int, string>(){
+                                                                    {0,""},
+                                                                    {1,"One"},
+                                                                    {2,"Two"},
+                                                                    {3,"Three"},
+                                                                    {4,"Four"},
+                                                                    {5,"Five"},
+                                                                    {6,"Six"},
+                                                                    {7,"Seven"},
+                                                                    {8,"Eight"},
+                                                                    {9,"Nine"},
+                                                                    {10,"Ten"},
+                                                                    {11,"Eleven"},
+                                                                    {12,"Twelve"},
+                                                                    {13,"Thirteen"},
+                                                                    {14,"Fourteen"},
+                                                                    {15,"Fifteen"},
+                                                                    {16,"Sixteen"},
+                                                                    {17,"Seventeen"},
+                                                                    {18,"Eighteen"},
+                                                                    {19,"Nineteen"},
+                                                                    {20,"Twenty"},
+                                                                    {30,"Thirty"},
+                                                                    {40,"Forty"},
+                                                                    {50,"Fifty"},
+                                                                    {60,"Sixty"},
+                                                                    {70,"Seventy"},
+                                                                    {80,"Eighty"},
+                                                                    {90,"Ninety"},
+                                                                    {100,"Hundred"},
+                                                                    {1000,"Thousand"},
+                                                                    {1000000,"Million"},
+                                                                     };
+
+
+
+        public static string ToEnglishText(this int Number)
+        {
+            EnglishTextNumber = String.Empty;
+            Spiltconcatenation(Number);
+            return EnglishTextNumber.Trim();
+        }
+
+        private static void Spiltconcatenation(int number)
+        {
+            int[] numberArray = number.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+
+            int TenThousand = number%10000;
+
+            int Thousand = number%1000;
+
+            int Hundred = number%100;
+
+
+            if (TenThousand >= 10000)
+            {
+                WordBuilder(Convert.ToInt32(numberArray[0].ToString() + numberArray[1].ToString()));
+                WordBuilder(1000);
+                if (Thousand > 0)
+                    Spiltconcatenation(TenThousand);
+            }
+            else if (number >= 1000)
+            {
+                WordBuilder(numberArray[0]);
+                WordBuilder(1000);
+                if (Thousand > 0)
+                Spiltconcatenation(Thousand);
+            }else if (number >= 100)
+            {
+                WordBuilder(numberArray[0]);
+                WordBuilder(100);
+                if (Hundred > 0)
+                Spiltconcatenation(Hundred);
+            }else if (number >= 21)
+            {
+                WordBuilder(Convert.ToInt32(numberArray[0] + "0"));
+                WordBuilder(numberArray[1]);
+
+            }
+            else
+            {
+
+                WordBuilder(number);
+            }
+        }
+
+         private static void WordBuilder(int number){
+            EnglishTextNumber += " " + texTDictionary[number];
+        }
+
+    }
+}
---
+++ PrintNumbers/TestNumberToText.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PrintNumbers
+{
+
+    public class TestNumberToText
+    {
+
+     //   [TestCase(0, "")]
+        [TestCase(1, "One")]
+        [TestCase(2, "Two")]
+        [TestCase(3, "Three")]
+        [TestCase(4, "Four")]
+        [TestCase(5, "Five")]
+        [TestCase(6, "Six")]
+        [TestCase(7, "Seven")]
+        [TestCase(8, "Eight")]
+        [TestCase(9, "Nine")]
+        [TestCase(10, "Ten")]
+        [TestCase(11, "Eleven")]
+        [TestCase(12, "Twelve")]
+        [TestCase(13, "Thirteen")]
+        [TestCase(14, "Fourteen")]
+        [TestCase(15, "Fifteen")]
+        [TestCase(16, "Sixteen")]
+        [TestCase(17, "Seventeen")]
+        [TestCase(18, "Eighteen")]
+        [TestCase(19, "Nineteen")]
+        [TestCase(20, "Twenty")]
+        [TestCase(21, "Twenty One")]
+        [TestCase(30, "Thirty")]
+        [TestCase(40, "Forty")]
+        [TestCase(50, "Fifty")]
+        [TestCase(60, "Sixty")]
+        [TestCase(70, "Seventy")]
+        [TestCase(80, "Eighty")]
+        [TestCase(90, "Ninety")]
+        [TestCase(100, "One Hundred")]
+        [TestCase(1000, "One Thousand")]
+        [TestCase(8576, "Eight  Thousand,  Five  Hundred  Seventy  Six")]
+
+
+        public void ShouldConvertToEnglishText(int number, string englishText)
+        {
+            number.ToEnglishText().Should().Be(englishText);
+        }
+    }
+}
